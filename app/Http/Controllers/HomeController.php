8f17147dce46f4bed93f4bcaeb2df041954bfba2
tranthanhtuan269 <tran.thanh.tuan269@gmail.com>
2023-02-27 15:32:59 +0700

    css

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,20 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Word;
-use Inertia\Inertia;
+use Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
@@ -25,7 +15,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $words = Word::get();
-        return view('dashboard', ['words' => $words]);
+        if(Auth::check){
+            $words = Word::get();
+            return view('dashboard', ['words' => $words]);
+        }else{
+            return redirect('/login');
+        }
     }
 }
