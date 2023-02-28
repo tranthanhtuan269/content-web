f46c9d55f847ef8c26fd24787d1c4821375a6a21
tran thanh tuan <tran.thanh.tuan269@gmail.com>
2023-02-28 21:12:04 +0700

    css

--- resources/views/words/index.blade.php
+++ resources/views/words/index.blade.php
@@ -30,41 +30,6 @@
         </thead>
     </table>
 
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>S.No</th>
-                <th>Word</th>
-                <th>Language</th>
-                <th width="280px">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php
-                if(isset($_GET['page'])) {
-                    $page = (int) $_GET['page'];
-                }else {
-                    $page = 1;
-                }
-            @endphp
-            @foreach ($words as $key => $word)
-                <tr>
-                    <td>{{($page - 1) * 20 + $key + 1 }}</td>
-                    <td>{{ $word->word }}</td>
-                    <td>{{ $word->language->name }}</td>
-                    <td>
-                        <form action="{{ route('words.destroy',$word->id) }}" method="Post">
-                            <a class="btn btn-primary" href="/words/{{ $word->id }}/edit?language={{ isset($_GET['language']) ? $_GET['language'] : 1 }}">Edit</a>
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-        </tbody>
-    </table>
-    {!! $words->appends(request()->input()) !!}
     <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
@@ -83,17 +48,20 @@
                         name: 'action', 
                         render: function(data, type, row){
                             var html = '';
-                            html += '<form action="/words/'+row.id+'" method="Post">'
+                            html += '<form action="/words/'+row.id+'" id="form-'+row.id+'" method="Post">'
                             html += '<a class="btn btn-primary mx-2" href="/words/'+row.id+'/edit?language={{ isset($_GET['language']) ? $_GET['language'] : 1 }}">Edit</a>'
                             html += '@csrf';
                             html += '@method('DELETE')';
-                                html += '<button type="submit" class="btn btn-danger">Delete</button>'
+                                html += '<div class="btn btn-danger delete-btn" data-id="'+row.id+'">Delete</div>'
                             html += '</form>'
                             return html;
                         },
                         orderable: false
                     },
-                ]
+                ],
+                fnDrawCallback: function( oSettings ) {
+                    addEventListener();
+                },
             });
             
             $('#import-btn').click(() => {
@@ -105,6 +73,7 @@
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             });
+
             $("#myfile").on('change',(function(e) {
                 e.preventDefault();
                 var file_data = $('#myfile').prop('files')[0];
@@ -138,6 +107,37 @@
                     }
                 });
             }));
+
+            function addEventListener(){
+                $('.delete-btn').off('click');
+                $('.delete-btn').click(function(){
+                    var id = $(this).data('id');
+
+                    Swal.fire({
+                        @if(isset($_GET['language']))
+                            @if($_GET['language'] == 2)
+                                title: 'Bạn có chắc chắn?',
+                                text: "Bạn có chắc chắn muốn xóa?",
+                            @else
+                                title: 'Are you sure?',
+                                text: "Are you sure to delete it?",
+                            @endif
+                        @else
+                            title: 'Are you sure?',
+                            text: "Are you sure to delete !",
+                        @endif
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, delete it!'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            $('#form-' + id).submit();
+                        }
+                    })
+                })
+            }
 
             $('#change-language').change(function(){
                 window.location.href = "/words?language=" + $(this).val();
